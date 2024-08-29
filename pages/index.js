948d393d88f2f519f1aa93c8ba656618e6e149e4
mehrazrumman <mehrazrumman33@icloud.com>
2024-08-29 19:50:42 +0600

    update

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+function Home() {
+  const data = [
+    { name: 'Anik Bhai', age: 33 },
+    { name: 'Ishmam Bhai', age: 28 },
+    { name: 'Joy Bhai', age: 25 },
+    { name: 'Nuri Apu', age: 24 },
+  ];
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>This App Created To Test Page Routing</title>
+      
+      </Head>
+
+      <main className={styles.main}>
+        <h1>Welcome to Area 51</h1>
+        <h2>Soldiers</h2>
+
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((user, index) => (
+              <tr key={index}>
+                <td>{user.name}</td>
+                <td>{user.age}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    </div>
+  );
+}
+
+export default Home;
